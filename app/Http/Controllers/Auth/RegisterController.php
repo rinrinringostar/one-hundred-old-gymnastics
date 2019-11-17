3ecb510bb3b2ed207a98d2a21260baf5e1e5c3d7
TakashiSaitou <bookman2710@gmail.com>
2019-11-17 14:48:44 +0900

    ユーザー新規登録後、スタンプ画面へ。

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/stamp';
 
     /**
      * Create a new controller instance.
